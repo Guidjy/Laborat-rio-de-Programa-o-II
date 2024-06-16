aedb25c84915a879d501e78845df5cffac78c717
Guilherme P. Pimentel <165931160+Guidjy@users.noreply.github.com>
2024-06-16 16:59:46 -0300

    Update jogo_arv.c

--- T3/jogo_arv.c
+++ T3/jogo_arv.c
@@ -3,11 +3,12 @@
 #include <string.h>
 #include <stdbool.h>
 #include <ctype.h>
+#include <assert.h>
 #include "jogo_arv.h"
 #include "tecla.h"
 #include "tela.h"
 #include "abb.h"
-
+#include "lista.h"
 
 // cada palavra tem um tempo até ser inserida.
 struct _word
@@ -20,6 +21,9 @@ struct _word
 // estrutura de um nó da árvore
 struct _no_arv
 {
+    int lin;                // posição vertical do nó na tela
+    int col;                // posição horizontal do nó na tela
+    int pos_no_nivel;       // posição da árvore no nível
     char *palavra;          // string com a palavra armazenada no nó
     struct _no_arv *esq;    // ponteiro para o filho esquerdo
     struct _no_arv *dir;    // ponteiro para o filho direito
@@ -91,8 +95,11 @@ int processa_entrada(char *palavra, char letra)
     // se o caractere digitado for "ESC"
     else if (letra == '\033')
     {
+        tela_limpa();
+        tela_lincol(12, 40);
         // pede confirmação do jogador para cancelar o jogo
         printf("Tem certeza que quer cancelar o jogo[s/n]? \n");
+        tela_atualiza();
         char confirmacao;
         do 
         {
@@ -131,19 +138,8 @@ void preenche_lista_de_silabas(char silabas[104][5])
         // (ex: silaba[0] = 's' >>> "sa", "se", "si"...)
         for (int j = 0; j < 5; j++)
         {
-            // trata silabas que começam com "Q" (qua, que, qui, quo)
-            if (silaba[0] == 'q')
-            {
-                if (vogais[j] == 'u') continue;
-                silaba[1] = 'u';
-                silaba[2] = vogais[j];
-            }
-            // trata o restante das sílabas
-            else
-            {
-                silaba[1] = vogais[j];
-                silaba[2] = '\0';
-            }
+            silaba[1] = vogais[j];
+            silaba[2] = '\0';
             // lista de silabas recebe a silaba
             strcpy(silabas[n_silabas], silaba);
             n_silabas++;
@@ -156,6 +152,7 @@ Palavra gera_palavra_aleatoria(char silabas[104][5], double t_min, double t_max)
 {
     // palavra a ser retornada
     Palavra nova_palavra = (Palavra) malloc(sizeof(struct _word));
+    assert(nova_palavra != NULL);
 
     // sorteia o número de sílabas (entre 1 e 5)
     int n_silabas = (rand() % 5) + 1;
@@ -165,6 +162,7 @@ Palavra gera_palavra_aleatoria(char silabas[104][5], double t_min, double t_max)
         int pos = rand() % 104;
         strcat(nova_palavra->palavra, silabas[pos]);
     }
+    strcat(nova_palavra->palavra, "\0");
 
     // sorteia um tempo até a inserção
     nova_palavra->tempo = (double)((rand() % (int)t_max) + (int)t_min);
@@ -176,33 +174,120 @@ Palavra gera_palavra_aleatoria(char silabas[104][5], double t_min, double t_max)
 void diminui_limites_de_tempo(double duracao, double *t_max, double *t_min)
 {    
     *t_max -= 0.0002 * duracao;
-    if (*t_max < 4.0)
+    if (*t_max < 2.0)
     {
         *t_max = 3.0;
     }
     *t_min -= 0.0001 * duracao;
-    if (*t_min < 2.0)
+    if (*t_min < 1.0)
     {
-        *t_min = 1.0;
+        *t_min = 1.5;
     }
 }
 
 
-// retorna a dimensão (espaço) horizontal necessária para o desenho da árvore iniciada nesse nó
-int calculo_dimensional(arv a, int nivel, int *pos_x)
+static void get_string(char s[20])
 {
-    // a     : nó de uma árvore
-    // nivel : nível do nó 
-    // pos_x : a posição horizontal onde o desenho dessa árvore deve iniciar
+    char string[20];
+    char caractere;
+    int i = 0;
+    while (true)
+    {
+        caractere = tecla_le_char();
+        if (caractere == '\n' || i == 19)
+        {
+            break;
+        }
+        else if (!isalpha(caractere))
+        {
+            continue;
+        }
+        else
+        {
+            string[i] = caractere;
+            i++;
+            printf("%c", caractere);
+        }
+    }
+    string[i] = '\0';
+    strcpy(s, string);
+}
 
-    // Um nó vazio não atualiza nada e ocupa espaço zero
-    if (arv_vazia(a))
+
+void atualiza_top5(int pontuacao)
+{
+    // Arquivo com as 5 maiores pontuações
+    FILE *top5;
+    // abre o arquivo para leitura
+    top5 = fopen("top5.csv", "r");
+    if (top5 == NULL)
     {
-        return pos_x;
+        printf("ERRO NA ABERTURA DO ARQUIVO\n");
+        return;
     }
-    // um nó não vazio ocupa o espaço de seus filhos mais o seu espaço
-    else
+    // listas para armazenar os dados do arquivo
+    Lista top5_nomes = lista_cria();   // armazena os nomes dos jogadores
+    Lista top5_pontos = lista_cria();  // armazena os pontos dos jogadores
+    char buffer_nome[20];
+    int buffer_pontuacao;
+    // lê os dados do arquivo
+    while (fscanf(top5, "%s %d", buffer_nome, &buffer_pontuacao) != EOF)
     {
-        return strlen(a->palavra) + calculo_dimensional(a->esq, nivel + 1, pos_x) + calculo_dimensional(a->dir, nivel + 1, pos_x);
+        // insere-os nas listas
+        lista_enqueue(top5_nomes, buffer_nome, "string");
+        lista_enqueue(top5_pontos, &buffer_pontuacao, "int");
     }
+    fclose(top5);
+    // abre o arquivo para escrever
+    top5 = fopen("top5.csv", "w");
+    if (top5 == NULL)
+    {
+        printf("ERRO NA ABERTURA DO ARQUIVO\n");
+        return;
+    }
+    // atualiza o top 5
+    int n_jogadores = lista_n_elem(top5_nomes);
+    // se o arquivo estiver vazio
+    if (n_jogadores == 0)
+    {
+        // pede o nome do jogador atual
+        char nome_jogador[20];
+        printf("Parabéns! Você está entre os 5 melhores!\n");
+        printf("Digite seu nome: \n");
+        tela_atualiza();
+        get_string(nome_jogador);
+        // escreve seu nome e pontuação no arquivo
+        fprintf(top5, "%s, %d\n", nome_jogador, pontuacao);
+    }
+    // mudar TODO
+    for (int i = 0; i < n_jogadores; i++)
+    {
+        strcpy(buffer_nome, (char*)lista_dequeue(top5_nomes));
+        buffer_pontuacao = *(int*)lista_dequeue(top5_pontos);
+        // verifica se a pontuação dessa partida foi maior do que uma do top 10
+        if (pontuacao > buffer_pontuacao)
+        {
+            // se sim, pede o nome do jogador atual
+            char nome_jogador[20];
+            printf("Parabéns! Você está entre os 5 melhores!\n");
+            printf("Digite seu nome: \n");
+            tela_atualiza();
+            get_string(nome_jogador);
+            // escreve seu nome e pontuação no arquivo
+            fprintf(top5, "%s, %d\n", nome_jogador, pontuacao);
+            // zera a pontuação para que ela não seja inserida denovo
+            pontuacao = 0;
+            i++;
+        }
+        // verifica novamente se ja não foram escritos 5 nomes
+        if (i < n_jogadores)
+        {
+            fprintf(top5, "%s %d\n", buffer_nome, buffer_pontuacao);
+        }
+    }
+    // fecha o arquivo e salva os dados
+    fclose(top5);
+    // libera as listas
+    lista_libera(top5_nomes);
+    lista_libera(top5_pontos);
 }

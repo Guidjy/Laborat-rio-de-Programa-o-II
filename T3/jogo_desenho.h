1b38a607eb6ac3763277de0c481ac54faef36934
Guilherme P. Pimentel <165931160+Guidjy@users.noreply.github.com>
2024-06-16 17:01:00 -0300

    Create jogo_desenho.h

---
+++ T3/jogo_desenho.h
@@ -0,0 +1,13 @@
+#include "abb.h"
+
+// desenha a árvore
+void desenha_arvore(arv self, int largura);
+
+// desenha o tempo de duração do jogo
+void desenha_duracao(double tempo);
+
+// imprime a palavra a ser inserida
+void desenha_palavra_a_inserir(char *palavra, double tempo);
+
+// desenha a borda inferior da tela e a palavra sendo digitada pelo jogador
+void desenha_borda(char *entrada, int pontuacao);

91b68e66bc2ac34790231cadb5e2c7412edff557
Guilherme P. Pimentel <165931160+Guidjy@users.noreply.github.com>
2024-07-17 11:42:51 -0300

    Update main.c

--- t4/main.c
+++ t4/main.c
@@ -1,39 +1,16 @@
+#include "rede.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "grafo.h"
+#include <time.h>
 
-int main()
+int main(int argc, char *argv[])
 {
-    // cria um grafo com nós e de arestas com tamanho de int
-    printf(">Cria um grafo: \n");
-    Grafo g = grafo_cria(sizeof(int), sizeof(int));
-
-    int pdado = 0;
-    grafo_insere_no(g, &pdado);
-    grafo_insere_no(g, &pdado);
-    grafo_insere_no(g, &pdado);
-    grafo_insere_no(g, &pdado);
-    grafo_altera_valor_aresta(g, 0, 1, &pdado);
-    grafo_altera_valor_aresta(g, 1, 2, &pdado);
-    grafo_altera_valor_aresta(g, 2, 3, &pdado);
-    grafo_imprime(g);
-    
-    // checa ciclos
-    if (grafo_tem_ciclo(g))
-    {
-        printf("Tem ciclo\n");
-    }
-    else
-    {
-        printf("Nao tem ciclo\n");
-    }
-
-    //ordem topológica
-    Fila ordem_topologica = fila_cria(sizeof(int));
-    ordem_topologica = grafo_ordem_topologica(g);
-    fila_imprime(ordem_topologica);
-
-    fila_destroi(ordem_topologica);
-    grafo_destroi(g);
-    return 0;
+  srand(time(0));
+  Rede rede = NULL;
+  if (argc > 1) {
+    rede = rede_cria(argv[1]);
+  }
+  int pontos = joga_com_tela(rede);
+  printf("pontos: %d\n", pontos);
+  rede_destroi(rede);
 }
